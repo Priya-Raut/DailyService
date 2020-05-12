7c55ff8294550be97ae79aa917e32df8d83c22da
Priya Raut <rautpriya215@gmail.com>
2020-05-12 18:18:15 +0530

    Add DatabaseConnectionManager.

---
+++ DailyService/src/com/dailyservice/storage/DatabaseConnectionManager.java
@@ -0,0 +1,44 @@
+package com.dailyservice.storage;
+
+import java.sql.*;
+
+public class DatabaseConnectionManager {
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306";
+    private static final String USER = "root";
+    private static final String PASSWORD = "MySQL215";
+    private static final String TEST_QUERY = "Select * from Item";
+
+    /**
+     * Establish connection with database.
+     *
+     * @return Connection object if database connection is established, null otherwise.
+     */
+    public static Connection getConnection(){
+        Connection connection = null;
+        try{
+            Class.forName(DRIVER_NAME);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            return connection;
+        }catch (SQLException | ClassNotFoundException throwable){
+            throwable.printStackTrace();
+        }
+        return connection;
+    }
+
+    /**
+     * Test database connection by running a select query.
+     */
+    public void testConnection(){
+        Statement statement = null;
+        try {
+            statement = DatabaseConnectionManager.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(TEST_QUERY);
+            while(resultSet.next()){
+                System.out.println("Item Name: " + resultSet.getString(1) + " Item Price: " + resultSet.getDouble(2));
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+    }
+}
