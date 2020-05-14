a4eef2f7f53b0b0a828fe0e363538f2366358c6f
Priya Raut <rautpriya215@gmail.com>
2020-05-14 16:28:10 +0530

    Move DatabaseConnectionTester class to test package. Add Junit test to test connection.

---
+++ DailyService/src/test/java/com/dailyservice/storage/DatabaseConnectionTest.java
@@ -0,0 +1,21 @@
+package com.dailyservice.storage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+/**
+ * This class tests database connection.
+ *
+ * @author Priya Raut
+ * @version 1.0
+ */
+public class DatabaseConnectionTest {
+
+    @Test
+    public void shouldConnectToDatabase(){
+        Connection connection = DatabaseConnectionManager.getConnection();
+        Assert.assertTrue(connection != null);
+    }
+}
