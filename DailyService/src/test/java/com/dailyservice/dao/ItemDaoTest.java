17217da226eef6279cbff8b8e684758738cc0e12
Priya Raut <rautpriya215@gmail.com>
2020-05-14 17:13:32 +0530

    Add error messages in case tests using AssertTrue fails.

--- DailyService/src/test/java/com/dailyservice/dao/ItemDaoTest.java
+++ DailyService/src/test/java/com/dailyservice/dao/ItemDaoTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 
 public class ItemDaoTest {
     Connection connection;
+
     @Before
     public void setUp() throws Exception {
         connection = DatabaseConnectionManager.getConnection();
@@ -24,8 +25,17 @@ public class ItemDaoTest {
         ItemDao dao = new ItemDao();
         String itemName = "Salt";
         BigInteger itemId = dao.getItemIdFromItemName(connection, itemName);
-        Assert.assertTrue("An itemId has been received successfully", itemId != BigInteger.valueOf(0));
+        Assert.assertTrue("An itemId does not exist in Item table for given item name.", itemId != BigInteger.valueOf(0));
     }
+
+    @Test
+    public void shouldNotGetItemIdFromItemName() {
+        ItemDao dao = new ItemDao();
+        String itemName = "Sugar";
+        BigInteger itemId = dao.getItemIdFromItemName(connection, itemName);
+        Assert.assertTrue(itemId == BigInteger.valueOf(0));
+    }
+
 
     @After
     public void tearDown() throws Exception {
---
+++ DailyService/src/test/java/com/dailyservice/resources/DeliveryOrderResourceTest.java
@@ -0,0 +1,12 @@
+package com.dailyservice.resources;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeliveryOrderResourceTest {
+
+    @Test
+    public void createDeliveryOrderDto() {
+    }
+}
