0192a130b9e7cf2bccc89df69d27748757feb544
Priya Raut <rautpriya215@gmail.com>
2020-05-14 17:42:46 +0530

    Add shouldGetProperBillAmountForGivenItems test.

--- DailyService/src/test/java/com/dailyservice/resources/DeliveryOrderResourceTest.java
+++ DailyService/src/test/java/com/dailyservice/resources/DeliveryOrderResourceTest.java
@@ -1,12 +1,35 @@
 package com.dailyservice.resources;
 
+import com.dailyservice.dto.DeliveryOrderDto;
+import com.dailyservice.dto.ItemDto;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class DeliveryOrderResourceTest {
 
     @Test
-    public void createDeliveryOrderDto() {
+    public void shouldGetProperBillAmountForGivenItems() {
+        List<ItemDto> items = new ArrayList<ItemDto>();
+        ItemDto item_1 = new ItemDto();
+        item_1.setItemName("Salt");
+        item_1.setItemPrice(25);
+        item_1.setItemQuantity(4);
+        items.add(item_1);
+
+        ItemDto item_2 = new ItemDto();
+        item_2.setItemName("Tea Powder");
+        item_2.setItemPrice(50);
+        item_2.setItemQuantity(1);
+        items.add(item_2);
+
+        DeliveryOrderDto deliveryOrderDto = new DeliveryOrderDto();
+        deliveryOrderDto.setBillAmount(items);
+
+        Assert.assertEquals(150, deliveryOrderDto.getBillAmount(), 0);
     }
 }
